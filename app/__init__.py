db5644af193fb26d4be7305c95f80df36c48e9d7
Zixuan Wang <zxw377@student.bham.ac.uk>
2024-08-27 14:24:37 +0100

    Initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -7,10 +7,13 @@ from flask_mailman import Mail
 from flask_migrate import Migrate
 from flask_socketio import SocketIO, join_room, send
 from flask_sqlalchemy import SQLAlchemy
+# from flask_wtf.csrf import CSRFProtect
+# from flask_wtf.csrf import generate_csrf
 
 app = Flask(__name__) # create flask app
 app.config['SECRET_KEY'] = b'WR#&f&+%78er0we=%799eww+#7^90-;s' # keep secret with flask sessions
-
+# Configure CSRF Protectio
+# csrf = CSRFProtect(app)
 
 # socketio
 socketio = SocketIO(app)
@@ -35,12 +38,12 @@ def on_join(data):
 
 
 
-# Password encode 密码加密
+# Password encode
 bcrypt = Bcrypt(app)
 
 
 # email confirm
-app.config['MAIL_SERVER'] = 'stmp.fastmail.com'
+app.config['MAIL_SERVER'] = 'smtp.fastmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
@@ -57,14 +60,14 @@ login_manager.init_app(app)
 login_manager.login_view = 'login_page'
 
 basedir = os.path.abspath(os.path.dirname(__file__)) # get the path of current file
-# 设置 SQL 数据库的 URI，数据库文件位于项目目录下的 data 文件夹中，文件名为 hooplife
+# Set the URI of the SQL database. The database file is located in the data folder under the project directory and the file name is hooplife
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:aqj710310@localhost/hooplife'
 
 from sqlalchemy.dialects.postgresql.base import PGDialect
 PGDialect._get_server_version_info = lambda *args: (9, 2)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://zixuan:uXwqAMRtJkmngBVlxFvKmA@hooplife-15923.8nj.gcp-europe-west1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full'
 # export DATABASE_URL=""
-# 禁用 SQLAlchemy 的修改追踪，以减少额外的内存开销。
+
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ueab1i7cjhbhng:p43658d39f9b45b652101bf45d3972e91782ff379c76c8288942c5de827ae4375@ce0lkuo944ch99.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/df2e9qtqtosjel'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ueab1i7cjhbhng:p43658d39f9b45b652101bf45d3972e91782ff379c76c8288942c5de827ae4375@ce0lkuo944ch99.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/df2e9qtqtosjel?sslmode=require'
@@ -75,9 +78,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
-
-
 # upload avatar
 app.config['UPLOAD_FOLDER'] = 'static/profile_pics'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # limit to 16MB
@@ -86,20 +86,25 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # limit to 16MB
 from app import views
 from app.model import *
 
-# 注册一个shell上下文处理器 便于频繁地在 Flask shell 中与数据库进行交互
+# Register a shell context processor to facilitate frequent interaction with the database in the Flask shell
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User,datetime=datetime, LocationModel=LocationModel,
                 HooplifeModel=HoopgameModel, MessageModel=MessageModel, ChatRoomModel=ChatRoomModel,
                 CommentModel=CommentModel)
 
-
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+# @app.context_processor
+# def inject_csrf_token():
+#     return dict(csrf_token=generate_csrf())
+
+# @app.context_processor
+# def context_processor():
+#     return {'csrf_token': generate_csrf()}  # This would cause the error you see.
 
 
 
